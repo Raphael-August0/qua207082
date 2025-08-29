f4b7073dc6785ccec4760958e7a8bd6c4cadc6f0
Raphael <raphael.augusto0309@gmail.com>
2025-08-28 21:43:56 -0300

    avanço em javascript

--- livro js/cap02/ex2_1.js
+++ livro js/cap02/ex2_1.js
@@ -1,7 +1,7 @@
 const frm = document.querySelector('form')
 const resp = document.querySelector('h3')
 
-frm.addEventListener("submit",(e)=>{
+frm.addEventListener("keyup",(e)=>{
     const nome = frm.inNome.value
     resp.innerText = `Olá ${nome}`
     e.preventDefault()
